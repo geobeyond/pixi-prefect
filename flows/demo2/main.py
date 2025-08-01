c5bb559bed2a55a672c895c02646012906b6e11d
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2025-08-01 19:55:38 +0100

    Adding prefect demo flows

--- flows/demo2/main.py
+++ flows/demo2/main.py
@@ -1,14 +1,18 @@
 import sys
 
+from cowpy import cow
 from prefect import flow
 
 
 @flow(log_prints=True)
 def my_workflow(name: str = "world") -> str:
+    my_cow = cow.get_cow()()
     message = F"Hello, {name.capitalize()}!"
+    formatted_message = my_cow.milk(message)
     print(f"running from environment at {sys.executable}")
+    print(formatted_message)
     return message
 
 
 if __name__ == "__main__":
-    my_workflow.serve(name="my_demo1_local_deployment")
+    my_workflow.serve(name="my_demo2_local_deployment")
